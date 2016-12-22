288781eff3725fd55c1ddece95be9f22fddfd324
Rob Tompkins <chtompki@gmail.com>
2016-12-22 15:55:27 -0500

    TEXT-32: LCS distance calculation

--- src/test/java/org/apache/commons/text/similarity/LongestCommonSubsequenceTest.java
+++ src/test/java/org/apache/commons/text/similarity/LongestCommonSubsequenceTest.java
@@ -1,19 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.commons.text.similarity;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by tompkicr on 12/20/16.
+ * Unit tests for {@link org.apache.commons.text.similarity.LongestCommonSubsequence}.
  */
 public class LongestCommonSubsequenceTest {
 
-    private LongestCommonSubsequence subject;
+    private static LongestCommonSubsequence subject;
 
-    @Before
-    public void setup() {
+    @BeforeClass
+    public static void setup() {
         subject = new LongestCommonSubsequence();
     }
 
