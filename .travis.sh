44c36828b5b335ee1df09d28a477ab9c88079157
Amey Jadiye <ameyjadiye@gmail.com>
2017-08-02 00:58:34 +0530

    using 64 bit edition

--- .travis.sh
+++ .travis.sh
@@ -47,13 +47,15 @@ echo "space on this drive:"
 
 df -h .
 
-wget https://iwm.dhe.ibm.com/sdfdl/1v2/regs2/linuxjavasdks/java/java8/8/0/3/12/linuxia32/Xa.2/Xb.N2_vvPzUu_2JEIyVPPqRybA1WF-OqrzhAy4sMq3E9ws/Xc.8/0/3/12/linuxia32/ibm-java-sdk-8.0-3.12-i386-archive.bin/Xd./Xf.LPr.D1vk/Xg.9247250/Xi.swg-sdk8/XY.regsrvs/XZ.SWELeiUcWhSb2NGHFYK0DaN0aBc/ibm-java-sdk-8.0-3.12-i386-archive.bin
+#wget https://iwm.dhe.ibm.com/sdfdl/1v2/regs2/linuxjavasdks/java/java8/8/0/3/12/linuxia32/Xa.2/Xb.N2_vvPzUu_2JEIyVPPqRybA1WF-OqrzhAy4sMq3E9ws/Xc.8/0/3/12/linuxia32/ibm-java-sdk-8.0-3.12-i386-archive.bin/Xd./Xf.LPr.D1vk/Xg.9247250/Xi.swg-sdk8/XY.regsrvs/XZ.SWELeiUcWhSb2NGHFYK0DaN0aBc/ibm-java-sdk-8.0-3.12-i386-archive.bin
 
-chmod 755 ibm-java-sdk-8.0-3.12-i386-archive.bin
+wget http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/8.0.4.7/linux/x86_64/ibm-java-sdk-8.0-4.7-x86_64-archive.bin
 
-printf "4\n1\n\n\n\n\nY\n\n\n\n" | ./ibm-java-sdk-8.0-3.12-i386-archive.bin
+chmod 755 ibm-java-sdk-8.0-4.7-x86_64-archive.bin
 
-export JAVA_HOME="`pwd`/ibm-java-i386-80"
+printf "4\n1\n\n\n\n\nY\n\n\n\n" | ./ibm-java-sdk-8.0-4.7-x86_64-archive.bin
+
+export JAVA_HOME="`pwd`/ibm-java-x86_64-80"
 export JDK_HOME="${JAVA_HOME}"
 export JAVAC="${JAVA_HOME}/bin/javac"
 export PATH="${JAVA_HOME}/bin:${PATH}"
