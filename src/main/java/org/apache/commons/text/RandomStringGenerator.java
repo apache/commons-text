99409ac1450dfeb1210d28ae80fb97eab6251406
Amey Jadiye <ameyjadiye@gmail.com>
2017-09-30 14:57:48 +0530

    adding validation for pair

--- src/main/java/org/apache/commons/text/RandomStringGenerator.java
+++ src/main/java/org/apache/commons/text/RandomStringGenerator.java
@@ -341,8 +341,10 @@ public final class RandomStringGenerator {
          * @return {@code this}, to allow method chaining.
          */
         public Builder withinRange(final char[] ... pairs) {
-            characterList = new ArrayList<>();
+            characterList = new ArrayList<Character>();
             for (final char[] pair :  pairs) {
+                Validate.isTrue(pair.length == 2,
+                      "Each pair must contain minimum and maximum code point");
                 final int minimumCodePoint = pair[0];
                 final int maximumCodePoint = pair[1];
                 Validate.isTrue(minimumCodePoint <= maximumCodePoint,
