377ea047b960ee76dd6946d8c2b6561583fcf033
Gary Gregory <ggregory@rocketsoftware.com>
2018-09-12 18:20:06 -0600

    Reuse constant instead of magic string.

--- src/main/java/org/apache/commons/text/lookup/UrlStringLookup.java
+++ src/main/java/org/apache/commons/text/lookup/UrlStringLookup.java
@@ -62,7 +62,7 @@ final class UrlStringLookup extends AbstractStringLookup {
         if (key == null) {
             return null;
         }
-        final String[] keys = key.split(":");
+        final String[] keys = key.split(SPLIT_STR);
         final int keyLen = keys.length;
         if (keyLen < 2) {
             throw IllegalArgumentExceptions.format("Bad URL key format [%s]; expected format is DocumentPath:Key.",
