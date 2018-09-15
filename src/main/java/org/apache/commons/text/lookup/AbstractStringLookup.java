d72b390c27c4dac9989c74628fe75bc20e561962
Gary Gregory <ggregory@rocketsoftware.com>
2018-09-15 11:25:52 -0600

    Refactor magic string.

--- src/main/java/org/apache/commons/text/lookup/AbstractStringLookup.java
+++ src/main/java/org/apache/commons/text/lookup/AbstractStringLookup.java
@@ -24,6 +24,7 @@ package org.apache.commons.text.lookup;
  */
 abstract class AbstractStringLookup implements StringLookup {
 
+    private static final String EMPTY = "";
     protected static final char SPLIT_CH = ':';
     protected static final String SPLIT_STR = String.valueOf(SPLIT_CH);
 
@@ -38,7 +39,7 @@ abstract class AbstractStringLookup implements StringLookup {
      */
     protected String substringAfter(final String value, final char ch) {
         final int indexOf = value.indexOf(ch);
-        return indexOf > -1 ? value.substring(indexOf + 1) : "";
+        return indexOf > -1 ? value.substring(indexOf + 1) : EMPTY;
     }
 
     /**
@@ -52,6 +53,6 @@ abstract class AbstractStringLookup implements StringLookup {
      */
     protected String substringAfter(final String value, final String str) {
         final int indexOf = value.indexOf(str);
-        return indexOf > -1 ? value.substring(indexOf + str.length()) : "";
+        return indexOf > -1 ? value.substring(indexOf + str.length()) : EMPTY;
     }
 }
