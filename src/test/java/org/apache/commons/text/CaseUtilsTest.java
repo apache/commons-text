e72bec3c84c582c4abbe02e1b81eec3c1373d365
Gary Gregory <garydgregory@gmail.com>
2019-12-19 09:25:09 -0500

    Checkstyle line length.

--- src/test/java/org/apache/commons/text/CaseUtilsTest.java
+++ src/test/java/org/apache/commons/text/CaseUtilsTest.java
@@ -72,7 +72,7 @@ public class CaseUtilsTest {
         assertThat(CaseUtils.toCamelCase("tocamelcase", false)).isEqualTo("tocamelcase");
 
         assertThat(CaseUtils.toCamelCase("\uD800\uDF00 \uD800\uDF02", true)).isEqualTo("\uD800\uDF00\uD800\uDF02");
-        assertThat(CaseUtils.toCamelCase("\uD800\uDF00\uD800\uDF01\uD800\uDF14\uD800\uDF02\uD800\uDF03", true, '\uD800', '\uDF14'))
-            .isEqualTo("\uD800\uDF00\uD800\uDF01\uD800\uDF02\uD800\uDF03");
+        assertThat(CaseUtils.toCamelCase("\uD800\uDF00\uD800\uDF01\uD800\uDF14\uD800\uDF02\uD800\uDF03", true, '\uD800',
+            '\uDF14')).isEqualTo("\uD800\uDF00\uD800\uDF01\uD800\uDF02\uD800\uDF03");
     }
 }
