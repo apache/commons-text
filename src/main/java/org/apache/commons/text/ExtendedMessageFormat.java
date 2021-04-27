a22b464406aa4c8b8919bc71d812e0da21680fc4
Arturo Bernal <arturobernalg@gmail.com>
2021-04-27 06:44:42 +0200

    TEXT-205 - Set void return method

--- src/main/java/org/apache/commons/text/ExtendedMessageFormat.java
+++ src/main/java/org/apache/commons/text/ExtendedMessageFormat.java
@@ -516,10 +516,9 @@ public class ExtendedMessageFormat extends MessageFormat {
      * @param pattern pattern to parse
      * @param pos current parse position
      * @param appendTo optional StringBuilder to append
-     * @return {@code appendTo}
      */
-    private StringBuilder appendQuotedString(final String pattern, final ParsePosition pos,
-            final StringBuilder appendTo) {
+    private void appendQuotedString(final String pattern, final ParsePosition pos,
+                                    final StringBuilder appendTo) {
         assert pattern.toCharArray()[pos.getIndex()] == QUOTE
                 : "Quoted string must start with quote character";
 
@@ -535,8 +534,11 @@ public class ExtendedMessageFormat extends MessageFormat {
             switch (c[pos.getIndex()]) {
             case QUOTE:
                 next(pos);
-                return appendTo == null ? null : appendTo.append(c, start,
-                        pos.getIndex() - start);
+                if (appendTo != null) {
+                    appendTo.append(c, start,
+                            pos.getIndex() - start);
+                }
+                return;
             default:
                 next(pos);
             }
