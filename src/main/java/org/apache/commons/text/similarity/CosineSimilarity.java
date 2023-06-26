dbd78d18bd5f3e254709a6cfd048a0e680bd3833
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-06-26 20:49:19 +0100

    simplified the CosineSimilarity class

--- src/main/java/org/apache/commons/text/similarity/CosineSimilarity.java
+++ src/main/java/org/apache/commons/text/similarity/CosineSimilarity.java
@@ -46,62 +46,27 @@ public class CosineSimilarity {
      * @return cosine similarity between the two vectors
      */
     public Double cosineSimilarity(final Map<CharSequence, Integer> leftVector,
-                                   final Map<CharSequence, Integer> rightVector) {
+                    final Map<CharSequence, Integer> rightVector) {
         if (leftVector == null || rightVector == null) {
             throw new IllegalArgumentException("Vectors must not be null");
         }
 
-        final Set<CharSequence> intersection = getIntersection(leftVector, rightVector);
+        double dotProduct = 0.0d;
+        double normA = 0.0d;
+        double normB = 0.0d;
 
-        final double dotProduct = dot(leftVector, rightVector, intersection);
-        double d1 = 0.0d;
-        for (final Integer value : leftVector.values()) {
-            d1 += Math.pow(value, 2);
+        for (Map.Entry<CharSequence, Integer> leftEntry : leftVector.entrySet()) {
+            normA += Math.pow(leftEntry.getValue(), 2);
+            if (rightVector.containsKey(leftEntry.getKey())) {
+                dotProduct += leftEntry.getValue() * rightVector.get(leftEntry.getKey());
+            }
         }
-        double d2 = 0.0d;
-        for (final Integer value : rightVector.values()) {
-            d2 += Math.pow(value, 2);
+
+        for (Integer rightValue : rightVector.values()) {
+            normB += Math.pow(rightValue, 2);
         }
-        final double cosineSimilarity;
-        if (d1 <= 0.0 || d2 <= 0.0) {
-            cosineSimilarity = 0.0;
-        } else {
-            cosineSimilarity = dotProduct / (Math.sqrt(d1) * Math.sqrt(d2));
-        }
-        return cosineSimilarity;
+
+        double denominator = Math.sqrt(normA) * Math.sqrt(normB);
+        return (denominator != 0) ? dotProduct / denominator : 0.0;
     }
-
-    /**
-     * Computes the dot product of two vectors. It ignores remaining elements. It means
-     * that if a vector is longer than other, then a smaller part of it will be used to compute
-     * the dot product.
-     *
-     * @param leftVector left vector
-     * @param rightVector right vector
-     * @param intersection common elements
-     * @return The dot product
-     */
-    private double dot(final Map<CharSequence, Integer> leftVector, final Map<CharSequence, Integer> rightVector,
-            final Set<CharSequence> intersection) {
-        long dotProduct = 0;
-        for (final CharSequence key : intersection) {
-            dotProduct += leftVector.get(key) * (long) rightVector.get(key);
-        }
-        return dotProduct;
-    }
-
-    /**
-     * Returns a set with strings common to the two given maps.
-     *
-     * @param leftVector left vector map
-     * @param rightVector right vector map
-     * @return common strings
-     */
-    private Set<CharSequence> getIntersection(final Map<CharSequence, Integer> leftVector,
-            final Map<CharSequence, Integer> rightVector) {
-        final Set<CharSequence> intersection = new HashSet<>(leftVector.keySet());
-        intersection.retainAll(rightVector.keySet());
-        return intersection;
-    }
-
 }
