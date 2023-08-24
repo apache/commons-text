ec0a2a1814cfd8aa8cc4560d7ef55fb555c4f1c0
Gary Gregory <garydgregory@gmail.com>
2023-08-24 10:21:32 -0400

    [TEXT-230] Javadoc of org.apache.commons.text.lookup.DefaultStringLookup.XML is incorrect

--- src/main/java/org/apache/commons/text/lookup/DefaultStringLookup.java
+++ src/main/java/org/apache/commons/text/lookup/DefaultStringLookup.java
@@ -41,7 +41,7 @@ public enum DefaultStringLookup {
     BASE64_DECODER(StringLookupFactory.KEY_BASE64_DECODER, StringLookupFactory.INSTANCE.base64DecoderStringLookup()),
 
     /**
-     * The lookup for Base64 decoding using the key {@code "base64Encoder"}.
+     * The lookup for Base64 encoding using the key {@code "base64Encoder"}.
      * @see StringLookupFactory#KEY_BASE64_ENCODER
      * @see StringLookupFactory#base64EncoderStringLookup()
      */
@@ -91,7 +91,7 @@ public enum DefaultStringLookup {
     JAVA(StringLookupFactory.KEY_JAVA, StringLookupFactory.INSTANCE.javaPlatformStringLookup()),
 
     /**
-     * The lookup for localhost information using the key {@code "localhost"}.
+     * The lookup for local host information using the key {@code "localhost"}.
      * @see StringLookupFactory#KEY_LOCALHOST
      * @see StringLookupFactory#localHostStringLookup()
      */
@@ -140,14 +140,14 @@ public enum DefaultStringLookup {
     URL_DECODER(StringLookupFactory.KEY_URL_DECODER, StringLookupFactory.INSTANCE.urlDecoderStringLookup()),
 
     /**
-     * The lookup for URL decoding using the key {@code "urlEncoder"}.
+     * The lookup for URL encoding using the key {@code "urlEncoder"}.
      * @see StringLookupFactory#KEY_URL_ENCODER
      * @see StringLookupFactory#urlEncoderStringLookup()
      */
     URL_ENCODER(StringLookupFactory.KEY_URL_ENCODER, StringLookupFactory.INSTANCE.urlEncoderStringLookup()),
 
     /**
-     * The lookup for URL decoding using the key {@code "xml"}.
+     * The lookup for XML decoding using the key {@code "xml"}.
      * @see StringLookupFactory#KEY_XML
      * @see StringLookupFactory#xmlStringLookup()
      */
