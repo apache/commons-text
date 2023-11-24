8cdae388c770c1a6dd9b77f0ba64984dadafba9a
Scient122 <l.lerose1@studenti.unisa.it>
2023-11-24 16:45:30 +0100

    Field made transient.

--- src/main/java/org/apache/commons/text/ExtendedMessageFormat.java
+++ src/main/java/org/apache/commons/text/ExtendedMessageFormat.java
@@ -113,7 +113,7 @@ public class ExtendedMessageFormat extends MessageFormat {
     /**
      * Our registry of FormatFactory.
      */
-    private final Map<String, ? extends FormatFactory> registry;
+    private transient final Map<String, ? extends FormatFactory> registry;
 
     /**
      * Constructs a new ExtendedMessageFormat for the default locale.
