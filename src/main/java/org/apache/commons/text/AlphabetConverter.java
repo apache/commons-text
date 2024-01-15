526b9418fd5946a89e9a9714225249fd983ad777
Gary Gregory <garydgregory@gmail.com>
2024-01-15 09:31:41 -0500

    Use /=

--- src/main/java/org/apache/commons/text/AlphabetConverter.java
+++ src/main/java/org/apache/commons/text/AlphabetConverter.java
@@ -201,7 +201,7 @@ public final class AlphabetConverter {
                 / (encodingCopy.size() - doNotEncodeCopy.size());
 
         while (lettersLeft / encodingCopy.size() >= 1) {
-            lettersLeft = lettersLeft / encodingCopy.size();
+            lettersLeft /= encodingCopy.size();
             lettersSoFar++;
         }
 
