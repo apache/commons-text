5ad4e8eee31eb683adcc4ccc1321868d45d5927d
Gary Gregory <garydgregory@gmail.com>
2024-08-03 08:46:03 -0400

    Format and fluent

--- src/main/java/org/apache/commons/text/StringSubstitutor.java
+++ src/main/java/org/apache/commons/text/StringSubstitutor.java
@@ -1557,12 +1557,28 @@ public class StringSubstitutor {
      */
     @Override
     public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append("StringSubstitutor [disableSubstitutionInValues=").append(disableSubstitutionInValues).append(", enableSubstitutionInVariables=")
-                .append(enableSubstitutionInVariables).append(", enableUndefinedVariableException=").append(enableUndefinedVariableException)
-                .append(", escapeChar=").append(escapeChar).append(", prefixMatcher=").append(prefixMatcher).append(", preserveEscapes=")
-                .append(preserveEscapes).append(", suffixMatcher=").append(suffixMatcher).append(", valueDelimiterMatcher=").append(valueDelimiterMatcher)
-                .append(", variableResolver=").append(variableResolver).append("]");
-        return builder.toString();
+        // @formatter:off
+        return new StringBuilder()
+            .append("StringSubstitutor [disableSubstitutionInValues=")
+            .append(disableSubstitutionInValues)
+            .append(", enableSubstitutionInVariables=")
+            .append(enableSubstitutionInVariables)
+            .append(", enableUndefinedVariableException=")
+            .append(enableUndefinedVariableException)
+            .append(", escapeChar=")
+            .append(escapeChar)
+            .append(", prefixMatcher=")
+            .append(prefixMatcher)
+            .append(", preserveEscapes=")
+            .append(preserveEscapes)
+            .append(", suffixMatcher=")
+            .append(suffixMatcher)
+            .append(", valueDelimiterMatcher=")
+            .append(valueDelimiterMatcher)
+            .append(", variableResolver=")
+            .append(variableResolver)
+            .append("]")
+            .toString();
+        // @formatter:on
     }
 }
