2f7f257eceb1cb0ef2cc3fac0f9087e3a7237f9a
Emmanuel Bourg <ebourg@apache.org>
2024-10-26 11:11:58 +0200

    Replaced redundant type casts

--- src/main/java/org/apache/commons/text/StringSubstitutor.java
+++ src/main/java/org/apache/commons/text/StringSubstitutor.java
@@ -499,7 +499,7 @@ public class StringSubstitutor {
      * Constructs a new instance with defaults for variable prefix and suffix and the escaping character.
      */
     public StringSubstitutor() {
-        this((StringLookup) null, DEFAULT_PREFIX, DEFAULT_SUFFIX, DEFAULT_ESCAPE);
+        this(null, DEFAULT_PREFIX, DEFAULT_SUFFIX, DEFAULT_ESCAPE);
     }
 
     /**
