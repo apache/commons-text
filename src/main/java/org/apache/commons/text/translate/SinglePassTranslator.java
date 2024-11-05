76ba1ecbc93b98ac7efcf0d6633a66d7deff5a6b
Gary Gregory <garydgregory@gmail.com>
2024-11-05 07:36:46 -0500

    Use preferred spelling for "cannot"

--- src/main/java/org/apache/commons/text/translate/SinglePassTranslator.java
+++ src/main/java/org/apache/commons/text/translate/SinglePassTranslator.java
@@ -43,7 +43,7 @@ abstract class SinglePassTranslator extends CharSequenceTranslator {
     public int translate(final CharSequence input, final int index, final Writer writer) throws IOException {
         if (index != 0) {
             throw new IllegalArgumentException(getClassName() + ".translate(final CharSequence input, final int "
-                    + "index, final Writer out) can not handle a non-zero index.");
+                    + "index, final Writer out) cannot handle a non-zero index.");
         }
 
         translateWhole(input, writer);
