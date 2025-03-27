ebf1c34da8422fa80fbdd5224ae4f2aafa62013a
Neha Karmakar <nkca1998@gmail.com>
2025-03-27 00:27:56 -0300

    Implementation Smell-2: Resolved Complex conditional

--- src/main/java/org/apache/commons/text/translate/NumericEntityUnescaper.java
+++ src/main/java/org/apache/commons/text/translate/NumericEntityUnescaper.java
@@ -16,13 +16,13 @@
  */
 package org.apache.commons.text.translate;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumSet;
-
-import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Translates XML numeric entities of the form &amp;#[xX]?\d+;? to
@@ -34,7 +34,9 @@ import org.apache.commons.lang3.ArrayUtils;
  */
 public class NumericEntityUnescaper extends CharSequenceTranslator {
 
-    /** Enumerates NumericEntityUnescaper options for unescaping. */
+    /**
+     * Enumerates NumericEntityUnescaper options for unescaping.
+     */
     public enum OPTION {
 
         /**
@@ -53,25 +55,29 @@ public class NumericEntityUnescaper extends CharSequenceTranslator {
         errorIfNoSemiColon
     }
 
-    /** Default options. */
+    /**
+     * Default options.
+     */
     private static final EnumSet<OPTION> DEFAULT_OPTIONS = EnumSet
-        .copyOf(Collections.singletonList(OPTION.semiColonRequired));
+            .copyOf(Collections.singletonList(OPTION.semiColonRequired));
 
-    /** EnumSet of OPTIONS, given from the constructor, read-only. */
+    /**
+     * EnumSet of OPTIONS, given from the constructor, read-only.
+     */
     private final EnumSet<OPTION> options;
 
     /**
      * Creates a UnicodeUnescaper.
-     *
+     * <p>
      * The constructor takes a list of options, only one type of which is currently
      * available (whether to allow, error or ignore the semicolon on the end of a
      * numeric entity to being missing).
-     *
+     * <p>
      * For example, to support numeric entities without a ';':
-     *    new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.semiColonOptional)
+     * new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.semiColonOptional)
      * and to throw an IllegalArgumentException when they're missing:
-     *    new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.errorIfNoSemiColon)
-     *
+     * new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.errorIfNoSemiColon)
+     * <p>
      * Note that the default behavior is to ignore them.
      *
      * @param options to apply to this unescaper
@@ -96,62 +102,92 @@ public class NumericEntityUnescaper extends CharSequenceTranslator {
     @Override
     public int translate(final CharSequence input, final int index, final Writer writer) throws IOException {
         final int seqEnd = input.length();
-        // Uses -2 to ensure there is something after the &#
-        if (input.charAt(index) == '&' && index < seqEnd - 2 && input.charAt(index + 1) == '#') {
-            int start = index + 2;
-            boolean isHex = false;
 
-            final char firstChar = input.charAt(start);
-            if (firstChar == 'x' || firstChar == 'X') {
-                start++;
-                isHex = true;
+        if (!isNumericEntityStart(input, index, seqEnd)) {
+            return 0;
+        }
 
-                // Check there's more than just an x after the &#
-                if (start == seqEnd) {
-                    return 0;
-                }
-            }
-
-            int end = start;
-            // Note that this supports character codes without a ; on the end
-            while (end < seqEnd && (input.charAt(end) >= '0' && input.charAt(end) <= '9'
-                                    || input.charAt(end) >= 'a' && input.charAt(end) <= 'f'
-                                    || input.charAt(end) >= 'A' && input.charAt(end) <= 'F')) {
-                end++;
-            }
-
-            final boolean semiNext = end != seqEnd && input.charAt(end) == ';';
-
-            if (!semiNext) {
-                if (isSet(OPTION.semiColonRequired)) {
-                    return 0;
-                }
-                if (isSet(OPTION.errorIfNoSemiColon)) {
-                    throw new IllegalArgumentException("Semi-colon required at end of numeric entity");
-                }
-            }
-
-            final int entityValue;
-            try {
-                if (isHex) {
-                    entityValue = Integer.parseInt(input.subSequence(start, end).toString(), 16);
-                } else {
-                    entityValue = Integer.parseInt(input.subSequence(start, end).toString(), 10);
-                }
-            } catch (final NumberFormatException nfe) {
+        int start = index + 2;
+        boolean isHex = isHexStart(input, start);
+        if (isHex) {
+            start++;
+            if (start == seqEnd) {
                 return 0;
             }
-
-            if (entityValue > 0xFFFF) {
-                final char[] chrs = Character.toChars(entityValue);
-                writer.write(chrs[0]);
-                writer.write(chrs[1]);
-            } else {
-                writer.write(entityValue);
-            }
-
-            return 2 + end - start + (isHex ? 1 : 0) + (semiNext ? 1 : 0);
         }
-        return 0;
+
+        int end = scanNumericEntityEnd(input, start, seqEnd);
+        boolean semiNext = hasSemicolon(input, end, seqEnd);
+
+        if (!semiNext) {
+            if (isSet(OPTION.semiColonRequired)) {
+                return 0;
+            }
+            if (isSet(OPTION.errorIfNoSemiColon)) {
+                throw new IllegalArgumentException("Semi-colon required at end of numeric entity");
+            }
+        }
+
+        final int entityValue;
+        try {
+            String numberStr = input.subSequence(start, end).toString();
+            entityValue = Integer.parseInt(numberStr, isHex ? 16 : 10);
+        } catch (final NumberFormatException nfe) {
+            return 0;
+        }
+
+        writeEntity(writer, entityValue);
+        return calculateConsumedLength(index, start, end, isHex, semiNext);
+    }
+
+    /**Extracted helper methods
+     *
+     * @param input
+     * @param index
+     * @param seqEnd
+     * @return
+     */
+
+    private boolean isNumericEntityStart(CharSequence input, int index, int seqEnd) {
+        return index < seqEnd - 2 &&
+                input.charAt(index) == '&' &&
+                input.charAt(index + 1) == '#';
+    }
+
+    private boolean isHexStart(CharSequence input, int start) {
+        char ch = input.charAt(start);
+        return ch == 'x' || ch == 'X';
+    }
+
+    private int scanNumericEntityEnd(CharSequence input, int start, int seqEnd) {
+        int end = start;
+        while (end < seqEnd && isHexDigit(input.charAt(end))) {
+            end++;
+        }
+        return end;
+    }
+
+    private boolean isHexDigit(char ch) {
+        return (ch >= '0' && ch <= '9') ||
+                (ch >= 'a' && ch <= 'f') ||
+                (ch >= 'A' && ch <= 'F');
+    }
+
+    private boolean hasSemicolon(CharSequence input, int end, int seqEnd) {
+        return end != seqEnd && input.charAt(end) == ';';
+    }
+
+    private void writeEntity(Writer writer, int entityValue) throws IOException {
+        if (entityValue > 0xFFFF) {
+            final char[] chrs = Character.toChars(entityValue);
+            writer.write(chrs[0]);
+            writer.write(chrs[1]);
+        } else {
+            writer.write(entityValue);
+        }
+    }
+
+    private int calculateConsumedLength(int index, int start, int end, boolean isHex, boolean semiNext) {
+        return 2 + (end - start) + (isHex ? 1 : 0) + (semiNext ? 1 : 0);
     }
 }
