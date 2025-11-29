f730ec119fe9a74c7a2316f50dd1993ce851b5ff
Gary Gregory <garydgregory@gmail.com>
2025-11-29 12:39:34 +0000

    Fix typo in local variable name

--- src/main/java/org/apache/commons/text/translate/CsvTranslators.java
+++ src/main/java/org/apache/commons/text/translate/CsvTranslators.java
@@ -47,13 +47,13 @@ public final class CsvTranslators {
 
         @Override
         void translateWhole(final CharSequence input, final Writer writer) throws IOException {
-            final String inputSting = input.toString();
-            if (StringUtils.containsNone(inputSting, CSV_SEARCH_CHARS)) {
-                writer.write(inputSting);
+            final String inputString = input.toString();
+            if (StringUtils.containsNone(inputString, CSV_SEARCH_CHARS)) {
+                writer.write(inputString);
             } else {
                 // input needs quoting
                 writer.write(CSV_QUOTE);
-                writer.write(StringUtils.replace(inputSting, CSV_QUOTE_STR, CSV_ESCAPED_QUOTE_STR));
+                writer.write(StringUtils.replace(inputString, CSV_QUOTE_STR, CSV_ESCAPED_QUOTE_STR));
                 writer.write(CSV_QUOTE);
             }
         }
