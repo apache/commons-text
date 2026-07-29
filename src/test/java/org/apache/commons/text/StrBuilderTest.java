9c2b569d52f71dc05c80776f537e9db43d9bbc1b
Maksym Korshun <korshunmax82@gmail.com>
2026-07-29 21:09:05 +0200

    [TEXT-241] Annotate deprecated StrBuilderTest

--- src/test/java/org/apache/commons/text/StrBuilderTest.java
+++ src/test/java/org/apache/commons/text/StrBuilderTest.java
@@ -45,6 +45,7 @@ import org.junit.jupiter.api.Test;
  *
  * @deprecated This class will be removed in 2.0.
  */
+@Deprecated
 class StrBuilderTest {
 
     private static final class MockReadable implements Readable {
